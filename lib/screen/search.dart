ee6dff9a5ec6ac01bbb9ab2aa248bfe7e00a03cb
flint <olusegun.sola19@gmail.com>
2021-10-21 22:15:36 +0100

    completed

--- lib/screen/search.dart
+++ lib/screen/search.dart
@@ -86,132 +86,139 @@ class SearchScreen extends StatelessWidget {
                 final singleProduct = productBloc.sugestion
                     .where((element) => element.title == search)
                     .toList();
-
-                return SizedBox(
-                  width: mobileWidth,
-                  height: mobileHeight,
-                  child: GridView.builder(
-                    padding: const EdgeInsets.all(8),
-                    physics: const NeverScrollableScrollPhysics(),
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2, childAspectRatio: 2 / 3),
-                    itemCount: singleProduct.length,
-                    itemBuilder: (ctx, idx) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          decoration: const BoxDecoration(
-                            boxShadow: [
-                              //background color of box
-                              BoxShadow(
-                                color: Colors.black12,
-                                blurRadius: 5.0, // soften the shadow
-                                spreadRadius: 1.0, //extend the shadow
-                              )
-                            ],
+                if (singleProduct.isEmpty) {
+                  return Container(
+                      child: Center(child: Text("Item not found")));
+                } else {
+                  return SizedBox(
+                    width: mobileWidth,
+                    height: mobileHeight,
+                    child: GridView.builder(
+                      padding: const EdgeInsets.all(8),
+                      physics: const NeverScrollableScrollPhysics(),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2, childAspectRatio: 2 / 3),
+                      itemCount: singleProduct.length,
+                      itemBuilder: (ctx, idx) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              boxShadow: [
+                                //background color of box
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  blurRadius: 5.0, // soften the shadow
+                                  spreadRadius: 1.0, //extend the shadow
+                                )
+                              ],
+                            ),
+                            height: 700,
+                            child: Stack(
+                              children: [
+                                Container(
+                                  height: 700,
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(9)),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.only(left: 8),
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  top: 3.0),
+                                              child: Text(
+                                                singleProduct[idx].title,
+                                                style: productTitleSty,
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  top: 1.0),
+                                              child: Text(
+                                                singleProduct[idx].desc,
+                                                style: productDescSty,
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  top: 5.0),
+                                              child: Text(
+                                                'N' +
+                                                    singleProduct[idx]
+                                                        .price
+                                                        .toString(),
+                                                style: productPriceSty,
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.all(8.0),
+                                              child: Container(
+                                                height: 40,
+                                                decoration: BoxDecoration(
+                                                    border: Border.all(
+                                                        color: const Color(
+                                                            0xffAD3BFC)),
+                                                    borderRadius:
+                                                        const BorderRadius.all(
+                                                            Radius.circular(
+                                                                10)),
+                                                    color: Colors.white),
+                                                width: double.infinity,
+                                                child: TextButton(
+                                                    child: const Text(
+                                                        'ADD TO CART',
+                                                        style: TextStyle(
+                                                            color: Color(
+                                                                0xff7A08FA),
+                                                            fontSize: 15,
+                                                            fontWeight:
+                                                                FontWeight
+                                                                    .bold)),
+                                                    onPressed: () {
+                                                      Navigator.pop(context);
+                                                    }),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Stack(
+                                    children: [
+                                      FittedBox(
+                                        clipBehavior: Clip.hardEdge,
+                                        child: Image.asset(
+                                          singleProduct[idx].image,
+                                          fit: BoxFit.cover,
+                                          width: 300,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
-                          height: 700,
-                          child: Stack(
-                            children: [
-                              Container(
-                                height: 700,
-                                decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(9)),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 8),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Padding(
-                                            padding:
-                                                const EdgeInsets.only(top: 3.0),
-                                            child: Text(
-                                              singleProduct[idx].title,
-                                              style: productTitleSty,
-                                            ),
-                                          ),
-                                          Padding(
-                                            padding:
-                                                const EdgeInsets.only(top: 1.0),
-                                            child: Text(
-                                              singleProduct[idx].desc,
-                                              style: productDescSty,
-                                            ),
-                                          ),
-                                          Padding(
-                                            padding:
-                                                const EdgeInsets.only(top: 5.0),
-                                            child: Text(
-                                              'N' +
-                                                  singleProduct[idx]
-                                                      .price
-                                                      .toString(),
-                                              style: productPriceSty,
-                                            ),
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: Container(
-                                              height: 40,
-                                              decoration: BoxDecoration(
-                                                  border: Border.all(
-                                                      color: const Color(
-                                                          0xffAD3BFC)),
-                                                  borderRadius:
-                                                      const BorderRadius.all(
-                                                          Radius.circular(10)),
-                                                  color: Colors.white),
-                                              width: double.infinity,
-                                              child: TextButton(
-                                                  child: const Text(
-                                                      'ADD TO CART',
-                                                      style: TextStyle(
-                                                          color:
-                                                              Color(0xff7A08FA),
-                                                          fontSize: 15,
-                                                          fontWeight:
-                                                              FontWeight.bold)),
-                                                  onPressed: () {
-                                                    Navigator.pop(context);
-                                                  }),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                              Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                child: Stack(
-                                  children: [
-                                    FittedBox(
-                                      clipBehavior: Clip.hardEdge,
-                                      child: Image.asset(
-                                        singleProduct[idx].image,
-                                        fit: BoxFit.cover,
-                                        width: 300,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                );
+                        );
+                      },
+                    ),
+                  );
+                }
               },
             ),
           ],
